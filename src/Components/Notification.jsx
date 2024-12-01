25cefbb051a03e9b4ae8c9dc2e7cfaa1ebe807b7
Saurabhchaudhary9799 <saurbhbailara@gmail.com>
2024-12-01 12:50:19 +0530

    again small change

--- src/Components/Notification.jsx
+++ src/Components/Notification.jsx
@@ -7,12 +7,12 @@ const Notification = () => {
   <div className="flex animate-scroll-continuous">
     <div className="flex min-w-full shrink-0 justify-center items-center">
       <p className="text-sm md:text-lg font-bold text-orange-500 px-4 whitespace-nowrap">
-        Early bird registration date has been extended upto 31.12.2024
+        Early bird registration date has been extended to 31.12.2024
       </p>
     </div>
     <div className="flex min-w-full shrink-0 justify-center items-center">
       <p className="text-sm md:text-lg font-bold text-orange-500 px-4 whitespace-nowrap">
-        Early bird registration date has been extended upto 31.12.2024
+        Early bird registration date has been extended to 31.12.2024
       </p>
     </div>
   </div>
